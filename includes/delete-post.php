54a9c61b85fc674f2a4051a40e580fde442b6127
sunneyman <open.air.xxi@gmail.com>
2019-04-01 00:29:20 +0300

    delete button function improvements

--- includes/delete-post.php
+++ includes/delete-post.php
@@ -2,7 +2,6 @@
 
 add_action( 'wp_ajax_custom_delete_post', 'custom_delete_post' );
 function custom_delete_post(){
-
     $permission = check_ajax_referer( 'custom_delete_post_nonce', 'nonce', false );
     if( $permission == false ) {
         echo 'error';
@@ -14,6 +13,43 @@ function custom_delete_post(){
     die();
 }
 
+add_action( 'wp_ajax_custom_find_posts', 'custom_find_posts' );
+function custom_find_posts(){
+
+    $user_id = $_REQUEST['id'];
+
+    $user = get_user_by( 'id', $user_id );
+
+    $args = array(
+        'author'        =>  $user_id,
+        'orderby'       =>  'post_date',
+        'order'         =>  'ASC',
+        'posts_per_page' => -1,
+        'include'     => array(),
+        'exclude'     => array(),
+        'meta_key'    => '',
+        'meta_value'  =>'',
+        'post_type'   => 'document',
+    );
+    $current_user_posts_array = get_posts( $args );
+
+    $users_posts = array();
+
+    foreach($current_user_posts_array as $post_data) {
+        array_push($users_posts, $post_data->post_name);
+    }
+
+    $report = (object) [
+        'user_id' => $user_id,
+        'user_role' => $user->roles[0],
+        'users_posts'  => $users_posts,
+    ];
+
+    echo json_encode( $report );;
+
+    die();
+}
+
 
 global $wp;
 $page = home_url(add_query_arg($wp->request));
@@ -21,13 +57,17 @@ $needle = "/documents/";
 
 if ( strpos( $page, $needle ) !== false ) :
 
-    wp_enqueue_script('delete-button', plugins_url() . '/simple-text-converter/public/delete-post-button.js', array('jquery'), 1.0, true);
+    wp_enqueue_script('delete-button', plugins_url() . '/simple-text-converter/public/delete-post-button.js', array('jquery'), 1.1, true);
 
     add_action( 'wp_enqueue_scripts', 'delete_button_ajax_data', 99 );
     function delete_button_ajax_data(){
+
+        $current_user_id = get_current_user_id();
+
         wp_localize_script( 'delete-button', 'delBtnAjax',
             array(
-                'url' => admin_url('admin-ajax.php')
+                'url' => admin_url('admin-ajax.php'),
+                'userId' => $current_user_id,
             )
         );
     }
--- public/delete-post-button.js
+++ public/delete-post-button.js
@@ -9,22 +9,13 @@
     }
 
 }(function($) {
+    var userId = delBtnAjax.userId;
+    jQuery(window).load(()=>addDeleteButton(userId));
 
 
     var clicker = jQuery('.delete-post-button>i');
 
     clicker.removeClass('invisible'); // if plugin is not available, trash icon will not be displayed
-
-        clicker.mouseenter((event)=>{
-            this.dropdown = document.createElement('div');
-            this.dropdown.innerHTML = "Remove Document";
-            this.dropdown.classList.add('delete-dropdown');
-            event.target.append(this.dropdown);
-        });
-
-        clicker.mouseleave((event)=>{
-            this.dropdown.remove();
-        });
 
     jQuery('.delete-post-button').click(function(event) {
 
@@ -32,59 +23,7 @@
         var nonce = event.target.attributes['data-nonce'].value;
         var elemHolder = event.target.parentNode;
 
-        console.log(nonce);
-
-        this.confirmMessage = document.createElement('div');
-        this.confirmMessage.classList.add('confirm-message');
-        this.confirmMessage.innerHTML="Are you sure? This may not be undone.";
-        document.body.append(this.confirmMessage);
-
-        this.buttonsHolder = document.createElement('div');
-        this.buttonsHolder.classList.add('confirm-buttons-holder');
-        this.confirmMessage.append(this.buttonsHolder);
-
-        this.confirmButton = document.createElement('div');
-        this.confirmButton.classList.add('confirm-button');
-        this.confirmButton.innerHTML="Sure!";
-        this.buttonsHolder.append(this.confirmButton);
-
-        this.cancelButton = document.createElement('div');
-        this.cancelButton.classList.add('cancel-button');
-        this.cancelButton.innerHTML="No, cancel.";
-        this.buttonsHolder.append(this.cancelButton);
-
-        this.cancelButton.addEventListener('click',(e)=>{
-            e.target.parentNode.parentNode.remove();
-        });
-
-
-        const confirmDelete = function(elemHolder) {
-
-                this.confirmMessage = document.createElement('div');
-                this.confirmMessage.classList.add('after-delete-message');
-                this.confirmMessage.innerHTML="Your document has been deleted.";
-                document.body.append(this.confirmMessage);
-
-                this.buttonsHolder = document.createElement('div');
-                this.buttonsHolder.classList.add('confirm-buttons-holder');
-                this.confirmMessage.append(this.buttonsHolder);
-
-                this.continueButton = document.createElement('div');
-                this.continueButton.classList.add('cancel-button');
-                this.continueButton.innerHTML="Press to continue";
-                this.buttonsHolder.append(this.continueButton);
-
-                elemHolder.parentNode.parentNode.parentNode.remove();
-
-                this.continueButton.addEventListener('click', (e)=>{
-                    window.location.reload(true);
-                });
-        };
-
-
         const deletePost = function(e){
-            // console.log('Delete confirmed!');
-            e.target.parentNode.parentNode.remove();
 
             var data = {
                 action: 'custom_delete_post',
@@ -97,21 +36,45 @@
                 url: delBtnAjax.url,
                 data: data,
                 beforeSend: function() {
-                    console.log(data,delBtnAjax.url);
                     elemHolder.classList.add( 'js-loading' );
                     elemHolder.parentNode.parentNode.classList.add( 'semi-opaque' );
                 },
                 complete: function (response) {
-                    console.log(response);
-                    confirmDelete(elemHolder);
+                    window.location.reload(true);
                 },
             });
-
             return false;
         };
-
-        this.confirmButton.addEventListener('click', deletePost);
-
+        deletePost();
     });
 
+
+    function addDeleteButton(userId) {
+        var data = {
+            action: 'custom_find_posts',
+            id: userId
+        };
+        jQuery.ajax({
+            method: 'GET',
+            url: delBtnAjax.url,
+            data: data,
+            complete: function (response) {
+                var respObj = JSON.parse(response.responseText);
+                setTrashButtons(respObj);
+            },
+        });
+    }
+
+    function setTrashButtons(respObj){
+        // console.log(respObj);
+        var postsOnPage = jQuery('.entity-document');
+
+        for(var i=0;i<postsOnPage.length; i++){
+            var postLink = postsOnPage[i].querySelector('a').href;
+            var postId = postLink.split("/");
+            if(respObj['user_role']==="administrator" || respObj['user_role']==="editor" || respObj['users_posts'].includes(postId[5])) {
+                // console.log('True!');
+            }
+        }
+    }
 }));
